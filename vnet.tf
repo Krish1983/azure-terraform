5f1b18769d226b5d7acd30062a6a39cc3ec4bd5a
krish1983 <kriskumar.k@gmail.com>
2026-03-03 14:01:16 +0000

    variable file and other changes

--- vnet.tf
+++ vnet.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet_demo" {
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
-  address_space       = ["10.0.0.0/16"]
-  name                = "vnet_demo"
+  address_space       = var.address_space
+  name                = var.vnet_name
    
   tags = {
     "Name" = "vent_demo"
